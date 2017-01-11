b7d4c9d7b5c12057aa9b74e4cd274cf603c070a9
YuanBro <15821448754@163.com>
2017-01-11 20:21:03 +0800

    Initial Commit

---
+++ PayProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayProject
+//
+//  Created by yuan on 2017/1/11.
+//  Copyright © 2017年 yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PayProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PayProject
+//
+//  Created by yuan on 2017/1/11.
+//  Copyright © 2017年 yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
